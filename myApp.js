805cf82314552de4ea507d9a774c41a9097f6053
HamidAhmed3 <hamid.ahmed3217@gmail.com>
2023-12-15 10:40:22 +0500

    Person Schema

--- myApp.js
+++ myApp.js
@@ -2,7 +2,18 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person; 
+let personSchema= new mongoose.Schema({
+  name : {
+    type : String,
+    required : true
+  },
+  age: Number,
+  favoriteFoods : [String]
+});
+
+let Person;
+
+Person = mongoose.model('Person'. personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/); 
