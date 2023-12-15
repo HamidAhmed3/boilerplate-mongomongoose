3f963904d4d37ab63e3816a4877facacb5534fe9
HamidAhmed3 <hamid.ahmed3217@gmail.com>
2023-12-15 09:46:57 +0500

    start

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let Person;
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  done(null /*, data*/); 
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
